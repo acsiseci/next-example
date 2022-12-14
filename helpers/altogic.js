df95f7506ccb0c9597fb50dd1f74873ffdd5ac91
acsiseci <acsiseci@gmail.com>
2022-12-14 19:28:32 +0300

    altogic build hatası

--- helpers/altogic.js
+++ helpers/altogic.js
@@ -1,9 +1,10 @@
-import { createClient } from "altogic";
+import { createClient } from "altogic"; // TODO Kaldıralacak
 import getConfig from 'next/config';
 
 let envUrl = getConfig().publicRuntimeConfig.altogicUrl;
 let clientKey = getConfig().publicRuntimeConfig.altogicClientKey;
 
 
-const altogic = createClient(envUrl, clientKey);
+const altogic = createClient(envUrl, clientKey);// TODO Kaldıralacak
+//const altogic = null // TODO Sadece Build Alırken Hata Çıkmaması İçin Eklenecek
 export default altogic;
