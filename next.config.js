10bf42101d0b16cbdb032a176f3ad1b8235d58c6
acsiseci <acsiseci@gmail.com>
2022-12-26 17:10:34 +0300

    next upgrade & add swcMinify

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: false,
   publicRuntimeConfig: {
     altogicUrl: 'https://rndw-re65.c4-na.altogic.com',
     altogicClientKey: '3244a5c2c87f4afa9e6dc0f6947c809f',
